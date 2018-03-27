885891d9cb14ad438d2044f26394ced995fc0aa3
Dave Harms <daveharmswebdev@gmail.com>
2018-03-27 17:01:32 -0500

    added service for choices

--- src/round-robin/round-robin.module.js
+++ src/round-robin/round-robin.module.js
@@ -3,6 +3,7 @@ import angular from 'angular';
 import { RRDisplay } from './rr-display/rr-display.component';
 import { DecisionComponent } from './rr-display/decisionView/decisionView.component';
 import { ChoiceViewComponent } from '../common/PresentationalComponents/choiceView/choiceView.component';
+import { RoundRobinService } from './round-robin.service';
 import { Choice } from '../common/Factories/choice.factory';
 import { Decision } from '../common/Factories/decision.factory';
 import { Survey } from '../common/Factories/survey.factory';
@@ -12,6 +13,7 @@ export const RoundRobinModule = angular
   .component('rrDisplay', RRDisplay)
   .component('decisionView', DecisionComponent)
   .component('choiceView', ChoiceViewComponent)
+  .service('RoundRobinService', RoundRobinService)
   .service('Choice', Choice)
   .service('Decision', Decision)
   .service('Survey', Survey)
